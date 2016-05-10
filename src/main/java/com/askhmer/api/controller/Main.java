a7595468cccd90dd429e0c58e90bee6ce914f7d2
Soklundy <bonglundy208@gmail.com>
2016-05-10 04:42:26 -0700

    testing2

--- src/main/java/com/askhmer/api/controller/Main.java
+++ src/main/java/com/askhmer/api/controller/Main.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Main {
 	
-	@RequestMapping(value = "/home")
+	@RequestMapping(value = "/hom")
 	public String homePage(){
 		return "home";
 	}
