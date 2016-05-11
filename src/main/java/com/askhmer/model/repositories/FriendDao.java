a7ce6f2c8087d9d58370eeb720917e0f16dfd79f
Soklundy <bonglundy208@gmail.com>
2016-05-10 21:43:35 -0700

    new

--- src/main/java/com/askhmer/model/repositories/FriendDao.java
+++ src/main/java/com/askhmer/model/repositories/FriendDao.java
@@ -1,5 +1,26 @@
 package com.askhmer.model.repositories;
 
+import java.util.List;
+
+import com.askhmer.model.dto.FriendDto;
+
+/***
+ * 
+ * @author soklundy
+ *
+ */
 public interface FriendDao {
 
+	/***
+	 * 
+	 * @return
+	 */
+	public List<FriendDto> listFriend();
+	
+	/***
+	 * 
+	 * @param friendDto
+	 * @return
+	 */
+	public boolean addFriend(FriendDto friendDto);
 }
--- src/main/java/com/askhmer/model/repositoriesImpl/FriendDaoImpl.java
+++ src/main/java/com/askhmer/model/repositoriesImpl/FriendDaoImpl.java
@@ -1,5 +1,23 @@
 package com.askhmer.model.repositoriesImpl;
 
-public class FriendDaoImpl {
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.askhmer.model.dto.FriendDto;
+import com.askhmer.model.repositories.FriendDao;
+
+@Repository
+public class FriendDaoImpl implements FriendDao{
+
+	public List<FriendDto> listFriend() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean addFriend(FriendDto friendDto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
