0f4d74224ec68c9d87eb94be63c2794d59c7cc7b
Soklundy <bonglundy208@gmail.com>
2016-05-11 04:41:42 -0700

    swagger

--- src/main/java/com/askhmer/config/WebConfig.java
+++ src/main/java/com/askhmer/config/WebConfig.java
@@ -17,9 +17,12 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mangofactory.swagger.plugin.EnableSwagger;
+
 @Configuration
 @ComponentScan(basePackages="com.askhmer")
 @EnableWebMvc
+@EnableSwagger
 @PropertySource("classpath:application.properties")
 public class WebConfig extends WebMvcConfigurerAdapter{
 	
