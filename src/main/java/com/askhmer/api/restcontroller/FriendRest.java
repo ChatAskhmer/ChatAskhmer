e1e26772ef236ae39ce7b06e65737943200be246
Soklundy <bonglundy208@gmail.com>
2016-05-10 20:52:09 -0700

    Friend

---
+++ src/main/java/com/askhmer/api/restcontroller/FriendRest.java
@@ -0,0 +1,14 @@
+package com.askhmer.api.restcontroller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/***
+ * 
+ * @author soklundy
+ *
+ */
+
+@RestController
+public class FriendRest {
+
+}
---
+++ src/main/java/com/askhmer/services/Friend.java
@@ -0,0 +1,12 @@
+package com.askhmer.services;
+
+/***
+ * 
+ * @author soklundy
+ *
+ */
+public interface Friend {
+	
+	
+
+}
